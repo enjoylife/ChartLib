01adf5f1ff5ffc7c97d94f9000daee4117dbeb19
Matt Clemens <matt.d.clemens@gmail.com>
2016-01-19 17:18:31 -0800

    initial bar chart ideas

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const TOPMARGIN = 20;
+export const LEFTMARGIN = 40;
+export const BOTTOMMARGIN = 30;
+export const RIGHTMARGIN = 20;
+
+export const MAXWIDTH = 700 ;
+export const MAXHEIGHT = 700 ;
+
+export const MINWIDTH = 300;
+export const MINHEIGHT = 300;
+
+export const margin = { top: TOPMARGIN, right: RIGHTMARGIN, bottom: BOTTOMMARGIN, left: LEFTMARGIN};
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    // must be 'source-map' or 'inline-source-map'
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract(
+                // activate source maps via loader query
+                'css?sourceMap!less?sourceMap')
+            }, {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    plugins: [// extract inline css into separate 'styles.css'
+        new ExtractTextPlugin('styles.css')]
+}
